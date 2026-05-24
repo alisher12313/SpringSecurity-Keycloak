85aa0234373fec3fbdf6cac4627a6a7d728dcea6
Alisher <alibobaladno@gmail.com>
2026-05-24 17:26:29 +0500

    add: postAuthorize and pre/postFilter

--- ex7/src/main/java/com/pm/ex7/controller/DemoController.java
+++ ex7/src/main/java/com/pm/ex7/controller/DemoController.java
@@ -2,10 +2,16 @@ package com.pm.ex7.controller;
 
 import com.pm.ex7.configuration.security.Demo4ConditionEvaluator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,5 +42,25 @@ public class DemoController {
     @PreAuthorize("@demo4ConditionEvaluator.hasAnyAuthorityReadAndCorrectUsername(#username, authentication)")
     public String demo4(@PathVariable(name = "username") String username) {
         return "Welcome " + username;
+    }
+
+    @GetMapping("/demo5")
+    @PostAuthorize("returnObject == 'demo 5'")
+    //executed after a service is done its job, use when restrict access to the value
+    //never use postAuthorize with methods that change data
+    public String demo5() {
+        return "demo5";
+    }
+
+    @GetMapping("/demo6")
+    @PreFilter("filterObject.contains('a')")
+    public String demo6(@RequestBody List<String> values) {
+        return "demo6";
+    }
+
+    @GetMapping("/demo7")
+    @PostFilter("filterObject.contains('a')")
+    public List<String> demo7() {
+        return List.of("aabb", "sadsa");
     }
 }

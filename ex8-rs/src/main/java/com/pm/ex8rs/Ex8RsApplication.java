5a6026c8b65c3a535139d874d8f6ae69c582a64e
Alisher <alibobaladno@gmail.com>
2026-05-30 01:11:08 +0500

    resource server for multitenancy

---
+++ ex8-rs/src/main/java/com/pm/ex8rs/Ex8RsApplication.java
@@ -0,0 +1,13 @@
+package com.pm.ex8rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex8RsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex8RsApplication.class, args);
+    }
+
+}
---
+++ ex8-rs/src/test/java/com/pm/ex8rs/Ex8RsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.ex8rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex8RsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

972955bde5e5ebaf3e6afd1e076f2510333fe558
Alisher <alibobaladno@gmail.com>
2026-06-01 00:43:35 +0500

    keycloak proj init

---
+++ keycloakex1/src/main/java/com/pm/keycloakex1/Keycloakex1Application.java
@@ -0,0 +1,13 @@
+package com.pm.keycloakex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Keycloakex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Keycloakex1Application.class, args);
+    }
+
+}

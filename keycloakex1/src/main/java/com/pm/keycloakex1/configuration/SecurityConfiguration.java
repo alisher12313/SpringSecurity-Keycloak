a460978b9d745c251985902cfe77e56e2ebb2308
Alisher <alibobaladno@gmail.com>
2026-06-01 01:12:39 +0500

    simple security conf of oauth2 resource server with keycloak

---
+++ keycloakex1/src/main/java/com/pm/keycloakex1/configuration/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.pm.keycloakex1.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Value("${app.security.oauth2-issuer-uri}")
+    private String issuerUri;
+
+    @Value("${app.security.oauth2-jwk-uri}")
+    private String jwkUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(auth -> {
+            auth.jwt(jwt -> jwt
+                    .decoder(jwtDecoder())
+                    .jwkSetUri(jwkUri)
+            );
+        });
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
+}
---
+++ keycloakex1/src/main/java/com/pm/keycloakex1/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.pm.keycloakex1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public String demo() {
+        return "Hello World";
+    }
+
+    @GetMapping("/demo2")
+    public String demo2() {
+        return "Demo2";
+    }
+}

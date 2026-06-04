aee9bbc9bc81f77fadb3e0aa6e0ed65b49b2107d
Alisher <alibobaladno@gmail.com>
2026-06-04 18:20:51 +0500

    b

--- keycloakex2/eventapp/src/main/java/com/pm/eventapp/configuration/SecurityConfiguration.java
+++ keycloakex2/eventapp/src/main/java/com/pm/eventapp/configuration/SecurityConfiguration.java
@@ -1,5 +1,18 @@
 package com.pm.eventapp.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableMethodSecurity
+@Configuration
 public class SecurityConfiguration {
 
     @Bean

d2cab5bfe27bc33d18db0385e2554831645b9289
Alisher <alibobaladno@gmail.com>
2026-06-04 18:20:41 +0500

    a

--- keycloakex2/eventapp/src/main/java/com/pm/eventapp/configuration/SecurityConfiguration.java
+++ keycloakex2/eventapp/src/main/java/com/pm/eventapp/configuration/SecurityConfiguration.java
@@ -1,4 +1,30 @@
 package com.pm.eventapp.configuration;
 
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(auth -> {
+            auth
+                    .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())
+            );
+        });
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/actuator/**").hasRole("eventapp-admin");
+            auth.requestMatchers("/api/v1/events/**").hasAnyRole("eventapp-user", "eventapp-admin");
+        });
+
+        http.sessionManagement(session -> {
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("http://localhost:9090/realms/proselyte");
+    }
 }
